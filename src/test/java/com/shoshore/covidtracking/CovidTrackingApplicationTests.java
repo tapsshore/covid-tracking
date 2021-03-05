816932aa4a696c818bbc280382cabf1b53bedca8
Tapiwanashe Shoshore <tapsshore@gmail.com>
2021-03-05 08:33:43 +0200

    first commit

---
+++ src/test/java/com/shoshore/covidtracking/CovidTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoshore.covidtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
